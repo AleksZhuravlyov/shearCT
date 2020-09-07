b96af7bbd206408145af83b975cea9d807685256
Aleksandr Zhuravlyov <a.s.zhuravljov@gmail.com>
2020-09-07 16:22:13 +0500

    shearCT: made minor enhancement of swelling script for set of files

--- scripts/swelling.py
+++ scripts/swelling.py
@@ -11,12 +11,12 @@ params = dict()
 params['imageAFileName'] = argv[1]
 params['valueName'] = 'array'
 
-params['shiftZ'] = float(0.001186)
-params['initZ'] = float(0.0008895)
-params['xCenter'] = float(0.00360544)
-params['yCenter'] = float(0.00364102)
-params['xWidth'] = float(0.0047)
-params['yWidth'] = float(0.0047)
+params['shiftZ'] = float(0.004)
+params['initZ'] = float(0.0005)
+params['xCenter'] = float(0.004)
+params['yCenter'] = float(0.004)
+params['xWidth'] = float(0.0025)
+params['yWidth'] = float(0.0025)
 params['nX'] = int(300)
 params['nY'] = int(300)
 
@@ -30,7 +30,9 @@ params['topConstraintsMin'] = [-30e-6, -30e-6, -30e-6, -0.2094, -0.2094, -0.2094
 params['topConstraintsMax'] = [30e-6, 30e-6, 30e-6, 0.2094, 0.2094, 0.2094, 1.01]
 
 answers = dict()
-for file in argv[1:3]:
+files = argv[2:]
+print(files)
+for file in files:
     params['imageBFileName'] = file
     answers[file[0:-3]] = swelling_schema(params)
 
